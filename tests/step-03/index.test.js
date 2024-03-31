562ab7bff0c23893caa130e8ede92414ceca49a4
“SHIVANI <shivanishivangi2004@gmail.com>
2024-03-31 23:42:38 +0530

    feat:test-4

--- tests/step-03/index.test.js
+++ tests/step-03/index.test.js
@@ -2,23 +2,13 @@ const readCSV = require('../../src/csvReader');
 const parseQuery = require('../../src/queryParser');
 
 test('Read CSV File', async () => {
-    const data = await readCSV('./sample.csv');
+    const data = await readCSV('./student.csv');
     expect(data.length).toBeGreaterThan(0);
     expect(data.length).toBe(3);
     expect(data[0].name).toBe('John');
     expect(data[0].age).toBe('30'); //ignore the string type here, we will fix this later
 });
 
-// test('Parse SQL Query', () => {
-//     const query = 'SELECT id, name FROM sample';
-//     const parsed = parseQuery(query);
-//     expect(parsed).toEqual({
-//         fields: ['id', 'name'],
-//         table: 'sample',
-//         whereClause: null
-//     });
-
-// });
 
 test('Parse SQL Query with Multiple WHERE Clauses', () => {
     const query = 'SELECT id, name FROM sample WHERE age = 30 AND name = John';
@@ -38,9 +28,3 @@ test('Parse SQL Query with Multiple WHERE Clauses', () => {
     });
 });
 
-// test('Execute SQL Query with Multiple WHERE Clause', async () => {
-//     const query = 'SELECT id, name FROM sample WHERE age = 30 AND name = John';
-//     const result = await executeSELECTQuery(query);
-//     expect(result.length).toBe(1);
-//     expect(result[0]).toEqual({ id: '1', name: 'John' });
-// });
